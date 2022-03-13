1efff573aeedf96dc0334039629a4fb2c9e0a585
dippa-1 <dominik.helfenstein@gmx.de>
2022-03-13 22:11:13 +0100

    format

--- Client_Sub/Receive.cpp
+++ Client_Sub/Receive.cpp
@@ -1,17 +1,17 @@
-#include <RCSwitch.h>
 #include "Receive.h"
 #include "output.h"
+#include <RCSwitch.h>
 
 RCSwitch mySwitch = RCSwitch();
 
-void receive_setup() {
-  mySwitch.enableReceive(D2); 
-}
+void receive_setup() { mySwitch.enableReceive(D2); }
 
 unsigned long receive_code() {
-    if(mySwitch.available()) {
-    output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),mySwitch.getReceivedProtocol());
+  if (mySwitch.available()) {
+    output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(),
+           mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(),
+           mySwitch.getReceivedProtocol());
     mySwitch.resetAvailable();
     return mySwitch.getReceivedValue();
-    }
+  }
 }
